3ebb4696068291fc710cbf694a28f60b4770e319
Aditi Raut <69860437+aditi-30@users.noreply.github.com>
2020-11-17 12:26:25 +0530

    Update sketch.js

--- project 20/sketch.js
+++ project 20/sketch.js
@@ -5,7 +5,7 @@ var speed;
 var weight;
 
 function setup() {
-  createCanvas(800,400);
+  createCanvas(1600,400);
 
   speed=random(55,90);
   weight=random(400,1500);
@@ -20,24 +20,24 @@ function setup() {
 }
 
 function draw() {
-  background(255,255,255);  
+  background(0);  
   if(wall.x-car.x<(car.width+wall.width)/2)
   {
 car.velocityX=0;
-var deformation=0.5*weight*speed*speed/22509;
+var deformation= (0.5*weight*speed*speed)/22509;
 if(deformation>180)
 {
   car.shapeColor=color(255,0,0);
 }
 if(deformation<180 && deformation>100)
 {
-  car.shapeColor=color(color(230,230,0)
-
+  car.shapeColor=color(230,230,0)
+}
 if(deformation<100)
 {
   car.shapeColor=color(0,255,0);
 }
   }
-}
+
   drawSprites();
 }
